18b9def061a3f58b799a251bcf90999613a061f3
james mwangi <jaymoh53@gmail.com>
2022-02-07 13:13:53 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    
+
     <div class="wrapper wrapper-full-page">
         <div class="full-page login-page" data-color="" data-image="{{url("img/background/background-2.jpg")}}">
             <!--   you can change the color of the filter page using: data-color="blue | azure | green | orange | red | purple" -->
