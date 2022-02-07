dab72d9ff64abc6fcbcfdb9a49419e616f3316f4
james mwangi <jaymoh53@gmail.com>
2022-02-07 14:26:59 +0300

    Update 2022_02_06_154558_equipment_inventory_equipement_required.php

--- database/migrations/2022_02_06_154558_equipment_inventory_equipement_required.php
+++ database/migrations/2022_02_06_154558_equipment_inventory_equipement_required.php
@@ -13,13 +13,13 @@ class EquipmentInventoryEquipementRequired extends Migration
      */
     public function up()
     {
-        Schema::create('equipment_inventory_required', function (Blueprint $table) {
+        Schema::create('equipment_inventory_equipment_required', function (Blueprint $table) {
             $table->integer('equipment_inventory_id')->unsigned();
-//            $table->foreign('equipment_inventory_id')->references('id')->on('equipment_inventory')
-//                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('equipment_inventory_id')->references('id')->on('equipment_inventory')
+                ->onUpdate('cascade')->onDelete('cascade');
             $table->integer('equipment_required_id')->unsigned();
-//            $table->foreign('equipment_required_id')->references('id')->on('equipment_required')
-//                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('equipment_required_id')->references('id')->on('equipment_required')
+                ->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
