ac11dc5ad2801685c991fdfd09c4d78bda579a4b
james mwangi <jaymoh53@gmail.com>
2022-02-07 20:48:42 +0300

    Update login.blade.php

--- resources/views/auth/v1/login.blade.php
+++ resources/views/auth/v1/login.blade.php
@@ -2,32 +2,48 @@
 
 @section('content')
     <div class="form-wrapper ">
-        <div class="text-left">
-        <h4>Login </h4>
-         <p>Enter your email or username and password</p>
-        </div>
-        <div class="username-paw">
         <!-- form -->
         <form>
-            <div class="form-group text-left">
-                <label>Username or email</label>
-                <input type="text" class="form-control" placeholder="Username or email" required autofocus>
-            </div>
-            <div class="form-group text-left">
-                <label>Password</label>
-                <input type="password" class="form-control" placeholder="Password" required>
-            </div>
-            <div class="form-group d-flex justify-content-between">
-                <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" checked="" id="customCheck1">
-                    <label class="custom-control-label" for="customCheck1">Remember me</label>
+            <div class="username-password">
+                <div class="text-left">
+                    <h4>Login </h4>
+                    <p>Enter your email or username and password</p>
                 </div>
-                <a href="#">Reset password</a>
+                <div class="form-group text-left">
+                    <label>Username or email</label>
+                    <input type="text" class="form-control" placeholder="Username or email" required autofocus>
+                </div>
+                <div class="form-group text-left">
+                    <label>Password</label>
+                    <input type="password" class="form-control" placeholder="Password" required>
+                </div>
+                <div class="form-group d-flex justify-content-between">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" checked="" id="customCheck1">
+                        <label class="custom-control-label" for="customCheck1">Remember me</label>
+                    </div>
+                    <a href="#">Reset password</a>
+                </div>
+                <button class="btn btn-primary btn-block btn-lg btn-uppercase">Let's go</button>
             </div>
-            <button class="btn btn-primary btn-block btn-lg btn-uppercase">Let's go</button>
-
+            <div class="otp">
+                <div class="form-group text-left">
+                    <h2>Two-factor authentication</h2>
+                    <p>An Email with your verification code has been sent to your phone and email address.</p>
+                    <label>6-character code</label>
+                    <input type="text" class="form-control" placeholder="6-character code" required autofocus>
+                </div>
+                <button class="btn btn-primary btn-block btn-lg btn-uppercase btn-rounded">Verify Code</button>
+                <div class="form-group ">
+                    <p>Didn't receive an SMS/Email?</p>
+                    <button class="btn btn-primary btn-block btn-rounded btn-uppercase">Resend</button>
+                </div>
+            </div>
         </form>
         <!-- ./ form -->
     </div>
-    </div>
+    <script>
+        $(".otp").hidden();
+
+    </script>
 @endsection
