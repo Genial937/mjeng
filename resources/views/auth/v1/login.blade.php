174b991f720fc483c61a03f19cea8e440ead16f7
james mwangi <jaymoh53@gmail.com>
2022-02-07 16:43:04 +0300

    Update login.blade.php

--- resources/views/auth/v1/login.blade.php
+++ resources/views/auth/v1/login.blade.php
@@ -15,23 +15,24 @@
         <!-- form -->
         <form>
             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1"
-                       aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with
-                    anyone else.
-                </small>
+                <label>Username or email</label>
+                <input type="text" class="form-control" placeholder="Username or email" required autofocus>
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1"
-                       placeholder="Password">
+                <label>Password</label>
+                <input type="password" class="form-control" placeholder="Password" required>
             </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div class="form-group d-flex justify-content-between">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" checked="" id="customCheck1">
+                    <label class="custom-control-label" for="customCheck1">Remember me</label>
+                </div>
+                <a href="#">Reset password</a>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button class="btn btn-primary btn-block">Sign in</button>
+            <hr>
+            <p class="text-muted">Don't have an account?</p>
+            <a href="register.html" class="btn btn-outline-light btn-sm">Register now!</a>
         </form>
         <!-- ./ form -->
     </div>
