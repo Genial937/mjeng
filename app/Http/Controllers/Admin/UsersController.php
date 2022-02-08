dbec5ea90d1e4d8ff061af1ffe714859204270dd
james mwangi <jaymoh53@gmail.com>
2022-02-08 10:44:26 +0300

    Update

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -15,26 +15,13 @@
     use Tymon\JWTAuth\Facades\JWTAuth;
     use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-    class AutheticationController extends Controller
+    class UsersController extends Controller
     {
 
         public function __construct() {
             $this->middleware('auth');
         }
-
-
-        //display
-        public function roles(Request $request)
-        {
-            if(auth()->check()):
-                $roles=Role::with(["users","permissions"])->get();
-
-                $permissions=Permission::with("roles")->get();
-                return view('auth.roles_permissions.roles',compact("roles",'permissions'));
-                else:
-                return redirect(route('login'));
-            endif;
-        }
+        
         //display
         public function usersView(Request $request)
         {
@@ -191,146 +178,7 @@
                 ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
             }
         }
-        //display
-        public function createRoleView(Request $request)
-        {
-            if(auth()->check()):
-                $roles=Role::with(["users","permissions"])->get();
-                $permissions=Permission::with("roles")->get();
-                return view('auth.roles_permissions.create-roles',compact("roles",'permissions'));
-            else:
-                return redirect(route('login'));
-            endif;
-        }
-        //create roles
-        public function createRole(Request $request)
-        {
-            $this->validate($request, [
-                "name" => "required|unique:roles|max:20|min:2|regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/",
-                "display_name" => "required|unique:roles|max:20|min:2|regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/",
-                "description"=>"required",
-                "permissions"=>"required"
-            ]);
 
-            try {
-                $role = new Role;
-                $role->name = $request->request->get('name'); // name of the new role
-                $role->display_name = $request->request->get('display_name');; // display name of the new role
-                $role->description = $request->request->get('description');
-                $role->save();
-                //assign role permisssion
-//                foreach ($request->permissions as $id):
-//                     $permission=Permission::find($id);
-//                    $role->attachPermission($permission);
-//                endforeach;
-                $role->permissions()->sync($request->request->get('permissions'));
 
-                return response()->json([
-                    'success' => true, 'message' => 'Role created successfully',
-                ], JsonResponse::HTTP_OK);
-            } catch (QueryException $e) {
-                // something went wrong
-                return response()->json([
-                    'success' => false,
-                    'errors' => ["roles"=>[$e->getMessage()]]
-                ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-            }
-        }
-        public function roleEditView(Request $request)
-        {
-            if(auth()->check()):
-                $permissions=Permission::with("roles")->get();
-                $role=Role::with(["users","permissions"])->find($request->route("id"));
-                return view('auth.roles_permissions.edit-roles',compact('role',"permissions"));
-            else:
-                return redirect(route('login'));
-            endif;
-        }
-        public function editRole(Request $request)
-        {
-            $this->validate($request, [
-                "id" => "required|exists:roles",
-            ]);
-
-            try {
-                Role::where('id',$request->id)->update($request->only([
-                    "name",
-                    "display_name",
-                    "description"
-                ]));
-                $role=Role::find($request->id);
-                $role->permissions()->sync($request->request->get('permissions'));
-
-                return response()->json([
-                    'success' => true, 'message' => 'Role updated successfully',
-                ], JsonResponse::HTTP_OK);
-            } catch (QueryException $e) {
-                // something went wrong
-                return response()->json([
-                    'success' => false,
-                    'errors' => ["roles"=>[$e->getMessage()]]
-                ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-            }
-        }
-        public function  deleteRole(Request $request)
-        {
-            $this->validate($request, [
-                'id' => 'required'
-            ]);
-            try {
-                Role::find($request->id)->delete();
-                return response()->json([
-                    'success' => true,
-                    'message' => 'Role delete successfully',
-                ], JsonResponse::HTTP_OK);
-
-            } catch (QueryException $e) {
-                // something went wrong
-                return response()->json([
-                    'success' => false,
-                    'errors' =>["exception"=>[ $e->getMessage()]]
-                ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-            }
-        }
-        //create permissions
-        //display
-        public function createPermissionView(Request $request)
-        {
-            if(auth()->check()):
-                $roles=Role::with(["users","permissions"])->get();
-                $permissions=Permission::with("roles")->get();
-                return view('auth.roles_permissions.create-permission',compact("roles",'permissions'));
-            else:
-                return redirect(route('login'));
-            endif;
-        }
-        public function createPermission(Request $request)
-        {
-            //to create permission, NB: kindly do some protective checking before saving, visit the Entrust documentation
-            //for more available options
-            $this->validate($request, [
-                "name" => "required|unique:permissions|max:20|min:2",
-                "display_name" => "required|unique:permissions|max:20|min:2",
-                "description"=>"required"
-            ]);
-            try {
-                $viewUsers = new Permission;
-                $viewUsers->name = $request->request->get('name'); // name of the new role
-                $viewUsers->display_name = $request->request->get('display_name');; // display name of the new role
-                $viewUsers->description = $request->request->get('description');
-                $viewUsers->save();
-
-                return response()->json([
-                    'success' => true, 'message' => 'Role created successfully'
-                ], JsonResponse::HTTP_OK);
-            } catch (QueryException $e) {
-                // something went wrong
-                return response()->json([
-                    'success' => false,
-                    'errors' => ["permission"=>[$e->getMessage()]]
-                ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-            }
-
-        }
 
     }
