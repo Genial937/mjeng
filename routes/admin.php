7b18df00afd63f0df18fd56522836cd54b477d28
james mwangi <jaymoh53@gmail.com>
2022-02-10 15:04:37 +0300

    Update

--- routes/admin.php
+++ routes/admin.php
@@ -17,6 +17,6 @@ Route::group(['prefix' => '/','middleware' => ['log.route']], function () {
     ]);
     Route::resource('projects', 'Admin\ProjectController')->names([
         'index' => 'admin.project',
-        'createDetails' => 'admin.project.create.details.view'
+        'createDetails' => 'admin.project.details'
     ]);
 });
