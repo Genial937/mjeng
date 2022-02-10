7066a204124e556be9d5967534ff1032abf81a22
james mwangi <jaymoh53@gmail.com>
2022-02-10 15:24:02 +0300

    Update details.blade.php

--- resources/views/admin/v1/project/create/details.blade.php
+++ resources/views/admin/v1/project/create/details.blade.php
@@ -31,23 +31,70 @@
                             <div id="wizard2">
                                 <h3>Project Details</h3>
                                 <section class="card card-body border mb-0">
-                                    <form id="form1">
-                                        <div class="form-group wd-xs-300">
-                                            <label>First name</label>
-                                            <input type="text" class="form-control" placeholder="First name" required>
-                                            <div class="valid-feedback">
-                                                Looks good!
+                                    <form class="needs-validation" novalidate>
+                                        <div class="form-row">
+                                            <div class="col-md-4 mb-3">
+                                                <label for="validationCustom01">First name</label>
+                                                <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
+                                                <div class="valid-feedback">
+                                                    Looks good!
+                                                </div>
                                             </div>
-                                        </div><!-- form-group -->
-                                        <div class="form-group wd-xs-300">
-                                            <label>Last name</label>
-                                            <input type="text" class="form-control" name="lastname"
-                                                   placeholder="Enter lastname"
-                                                   required>
-                                            <div class="valid-feedback">
-                                                Looks good!
+                                            <div class="col-md-4 mb-3">
+                                                <label for="validationCustom02">Last name</label>
+                                                <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
+                                                <div class="valid-feedback">
+                                                    Looks good!
+                                                </div>
                                             </div>
-                                        </div><!-- form-group -->
+                                            <div class="col-md-4 mb-3">
+                                                <label for="validationCustomUsername">Username</label>
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                                    </div>
+                                                    <input type="text" class="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" required>
+                                                    <div class="invalid-feedback">
+                                                        Please choose a username.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="col-md-6 mb-3">
+                                                <label for="validationCustom03">City</label>
+                                                <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
+                                                <div class="invalid-feedback">
+                                                    Please provide a valid city.
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 mb-3">
+                                                <label for="validationCustom04">State</label>
+                                                <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
+                                                <div class="invalid-feedback">
+                                                    Please provide a valid state.
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 mb-3">
+                                                <label for="validationCustom05">Zip</label>
+                                                <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
+                                                <div class="invalid-feedback">
+                                                    Please provide a valid zip.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                                                <label class="form-check-label" for="invalidCheck">
+                                                    Agree to terms and conditions
+                                                </label>
+                                                <div class="invalid-feedback">
+                                                    You must agree before submitting.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button class="btn btn-primary" type="submit">Submit form</button>
                                     </form>
                                 </section>
                                 <h3>Billing Information</h3>
@@ -58,7 +105,7 @@
                                 <h3>Payment Details</h3>
                                 <section class="card card-body border mb-0">
                                     <h5>Payment Details</h5>
-                                   
+
                                 </section>
                             </div>
 
