0cfa70cb2f764d230f18f95128af111778216733
james mwangi <jaymoh53@gmail.com>
2022-02-10 11:42:37 +0300

    Update

--- resources/views/admin/v1/includes/main_nav.blade.php
+++ resources/views/admin/v1/includes/main_nav.blade.php
@@ -1,28 +1,28 @@
 <div class="navigation">
     <div class="logo">
         <a href=index.html>
-            <img src="../../assets/media/image/logo.png" alt="logo">
+            <img src="{{asset("assets/media/image/logo_light.png")}}" alt="logo">
         </a>
     </div>
     <ul>
         <li>
-            <a  href="dashboard.html">
+            <a  href="{{route("admin.dashboard")}}">
                 <i class="nav-link-icon ti-pie-chart"></i>
                 <span class="nav-link-label">Dashboard</span>
-                <span class="badge badge-danger badge-small">2</span>
+
             </a>
         </li>
         <li>
-            <a  href="files.html">
-                <i class="nav-link-icon ti-file"></i>
-                <span class="nav-link-label">Files</span>
+            <a  href="#">
+                <i class="nav-link-icon ti-folder"></i>
+                <span class="nav-link-label">Projects</span>
             </a>
         </li>
         <li>
-            <a  href="activities.html">
-                <i class="nav-link-icon ti-pulse"></i>
-                <span class="nav-link-label">Activities</span>
-                <span class="badge badge-warning">New</span>
+            <a  href="#">
+                <i class="nav-link-icon ti-pie-chart"></i>
+                <span class="nav-link-label">Inventory</span>
+
             </a>
         </li>
         <li>
@@ -31,16 +31,10 @@
                 <span class="nav-link-label">Users</span>
             </a>
         </li>
-        <li class="flex-grow-1">
-            <a href="alert.html">
-                <i class="nav-link-icon ti-layers"></i>
-                <span class="nav-link-label">Components</span>
-            </a>
-        </li>
         <li>
             <a  href="settings.html">
                 <i class="nav-link-icon ti-settings"></i>
-                <span class="nav-link-label">Settings</span>
+                <span class="nav-link-label">Configurations</span>
             </a>
         </li>
     </ul>
