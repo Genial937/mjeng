44c8875205af058abb0ffbbec9d9d3ce9bb8b1c1
james mwangi <jaymoh53@gmail.com>
2022-02-14 17:20:00 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,28 +13,28 @@
     |
     */
 
-    Route::group(['prefix' => 'auth','middleware' => 'log.route'], function () {
-        Route::post('login', 'Api\V1\JwtAuthenticateController@authenticate');
-        Route::get('refresh', 'Api\V1\JwtAuthenticateController@refresh');
-        Route::get('me', 'Api\V1\JwtAuthenticateController@me');
-        Route::get('logout', 'Api\V1\JwtAuthenticateController@logout');
-        Route::post('change-password', 'Api\V1\JwtAuthenticateController@assistedChangePassword');
-    });
-    Route::group(['prefix' => 'admin', 'middleware' => 'log.route', ['ability:admin,create-users']], function () {
-        Route::post('role', 'Api\V1\JwtAuthenticateController@createRole');
-        Route::post('permission', 'Api\V1\JwtAuthenticateController@createPermission');
-        Route::get('roles', 'Api\V1\JwtAuthenticateController@roles');
-        Route::get('permissions', 'Api\V1\JwtAuthenticateController@permissions');
-        Route::post('attach-permission', 'Api\V1\JwtAuthenticateController@attachPermission');
-        Route::post('assign-role', 'Api\V1\JwtAuthenticateController@assignRole');
-        Route::post('user', 'Api\V1\JwtAuthenticateController@createUser');
-        Route::patch('user', 'Api\V1\JwtAuthenticateController@updateUser');
-        Route::get('user/{user_id}', 'Api\V1\JwtAuthenticateController@find');
-        Route::get('users', 'Api\V1\JwtAuthenticateController@index');
-        Route::post('change-user-password', 'Api\V1\JwtAuthenticateController@changePassword');
-    });
-
-
+//    Route::group(['prefix' => 'auth','middleware' => 'log.route'], function () {
+//        Route::post('login', 'Api\V1\JwtAuthenticateController@authenticate');
+//        Route::get('refresh', 'Api\V1\JwtAuthenticateController@refresh');
+//        Route::get('me', 'Api\V1\JwtAuthenticateController@me');
+//        Route::get('logout', 'Api\V1\JwtAuthenticateController@logout');
+//        Route::post('change-password', 'Api\V1\JwtAuthenticateController@assistedChangePassword');
+//    });
+//    Route::group(['prefix' => 'admin', 'middleware' => 'log.route', ['ability:admin,create-users']], function () {
+//        Route::post('role', 'Api\V1\JwtAuthenticateController@createRole');
+//        Route::post('permission', 'Api\V1\JwtAuthenticateController@createPermission');
+//        Route::get('roles', 'Api\V1\JwtAuthenticateController@roles');
+//        Route::get('permissions', 'Api\V1\JwtAuthenticateController@permissions');
+//        Route::post('attach-permission', 'Api\V1\JwtAuthenticateController@attachPermission');
+//        Route::post('assign-role', 'Api\V1\JwtAuthenticateController@assignRole');
+//        Route::post('user', 'Api\V1\JwtAuthenticateController@createUser');
+//        Route::patch('user', 'Api\V1\JwtAuthenticateController@updateUser');
+//        Route::get('user/{user_id}', 'Api\V1\JwtAuthenticateController@find');
+//        Route::get('users', 'Api\V1\JwtAuthenticateController@index');
+//        Route::post('change-user-password', 'Api\V1\JwtAuthenticateController@changePassword');
+//    });
+//
+//
 
 
 
