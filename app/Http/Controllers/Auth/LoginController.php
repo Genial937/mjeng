0665335374a2c77c86e90ed5a13e75c185bea970
james mwangi <jaymoh53@gmail.com>
2022-02-14 16:23:07 +0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('auth')->except('index');
     }
     use AuthenticatesUsers;
 
