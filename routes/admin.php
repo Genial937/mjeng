992ef8c3fb99fb66c58b641bf4c057ac4b6d53fb
james mwangi <jaymoh53@gmail.com>
2022-02-14 16:43:43 +0300

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -11,10 +11,12 @@
 |
 */
 
+Route::get('/dashboard', [
+    'as' => 'admin.dashboard',
+    'uses' => 'Admin\ProjectController@index'
+]);
 Route::group(['prefix' => '/','middleware' => ['log.route']], function () {
-    Route::resource('dashboard', 'Admin\DashboardController')->names([
-        'index' => 'admin.dashboard'
-    ]);
+
     Route::resource('projects', 'Admin\ProjectController')->names([
         'index' => 'admin.project',
         'createDetails' => 'admin.create'
