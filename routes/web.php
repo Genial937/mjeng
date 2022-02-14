7a6cd6e7fb53259dd1c8c73deea9ba11329f99a5
james mwangi <jaymoh53@gmail.com>
2022-02-14 15:44:53 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', [
     'as' => 'web.login',
     'uses' => 'Auth\LoginController@index'
 ]);
-
-Auth::routes();
 Route::group(['prefix' => 'auth','middleware' => ['log.route','user.type']], function () {
     Route::resource('login', 'Auth\LoginController')->names([
         'index' => 'web.login',
