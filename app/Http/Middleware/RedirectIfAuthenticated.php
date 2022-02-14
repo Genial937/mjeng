7810ea9061e4bec945c6120b3549dfb23b941017
james mwangi <jaymoh53@gmail.com>
2022-02-14 16:01:40 +0300

    AUth

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,18 +19,18 @@ class RedirectIfAuthenticated
     {
         if (Auth::guard($guard)->check()):
             //check user user type
-            $user=auth()->user();
-            if($user->user_type=="ADMIN"):
-                return redirect(route('admin.dashboard'));
-            elseif($user->user_type=="BUSINESS"):
-                return redirect(route('vendor.dashboard'));
-            elseif($user->user_type=="CONTRACTOR"):
-                return redirect(route('contractor.dashboard'));
-            else:
-                //unknown user
-                return redirect(route('web.error',"USER_404"));
-            endif;
-        endif;
+//            $user=auth()->user();
+//            if($user->user_type=="ADMIN"):
+//                return redirect(route('admin.dashboard'));
+//            elseif($user->user_type=="BUSINESS"):
+//                return redirect(route('vendor.dashboard'));
+//            elseif($user->user_type=="CONTRACTOR"):
+//                return redirect(route('contractor.dashboard'));
+//            else:
+//                //unknown user
+//                return redirect(route('web.error',"USER_404"));
+//            endif;
+//        endif;
         return $next($request);
     }
 }
