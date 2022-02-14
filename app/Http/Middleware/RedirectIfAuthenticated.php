51ba4ae0e5be2d389479b85cfb02e172e8150c9e
james mwangi <jaymoh53@gmail.com>
2022-02-14 16:22:10 +0300

    aUTH

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,13 +21,6 @@ class RedirectIfAuthenticated
             $user=auth()->user();
             if($user->user_type=="ADMIN"):
                 return redirect(route('admin.dashboard'));
-            elseif($user->user_type=="BUSINESS"):
-                return redirect(route('vendor.dashboard'));
-            elseif($user->user_type=="CONTRACTOR"):
-                return redirect(route('contractor.dashboard'));
-            else:
-                //unknown user
-                return redirect(route('web.error',"USER_404"));
             endif;
         }
 
